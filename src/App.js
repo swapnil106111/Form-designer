6da4018eb38a29238a8be6f82d4a63784b7a83a1
Swapnil106111 <swap106111@gmail.com>
2019-07-09 11:23:16 +0530

    added login component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Login from './component/Login';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Login />
       </div>
     );
   }
